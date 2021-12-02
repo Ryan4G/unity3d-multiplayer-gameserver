d4b531727ff1648573524b4ab888e0f44f1fbc24
ryan4gitee <rliang.lai@outlook.com>
2021-12-02 23:37:03 +0800

    Add scripts.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace unity3d_multiplayer_gameserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsgMove msgMove = new MsgMove();
+            msgMove.x = 100;
+            msgMove.y = 2;
+            msgMove.z = 3;
+
+            byte[] bytes = MsgBase.Encode(msgMove);
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
---
+++ Scripts/net/NetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+public class NetManager
+{
+    public static Socket listenfd;
+
+    public static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
+
+    static List<Socket> checkRead = new List<Socket>();
+}
